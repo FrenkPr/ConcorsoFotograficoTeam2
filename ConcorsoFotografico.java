a8e37686da5c70a9162d9392a0a4d21a56fd1b3e
FrenkPr <francescomochi2002@gmail.com>
2025-06-26 19:55:31 +0200

    Input voti
    Aggiunta sezione input voti + ulteriori commenti.
    Sostituito nella condizione del for, con il controllo delle foto premiate, il valore
    costante "5" con "fotoVotiGiudici.length" per rendere più dinamico il processo.
    Inoltre, ho corretto, sempre nello stesso for, il segno ">" a ">=" nella condizione.

--- ConcorsoFotografico.java
+++ ConcorsoFotografico.java
@@ -1,42 +1,52 @@
-package ConcorsoFotograficoTeam2;
-
-import javax.sound.sampled.SourceDataLine;
-
 import java.util.Scanner;
 
-public class ConcorsoFotografico {
-  public static void main(String[] args) {
-    // memorizza voti da 1 a 10 assegnati a 5 foto
-    // usa un ciclo for per inserire i voti uno alla volta
-
-    Scanner scannerNum = new Scanner(System.in);
-    
-    int[] fotoVotiGiudici = new int[5];
-
-
-
-    // stampare quali foto sono state premiate, se con voto >= a 8 (con operatore %)
-    int fotoNumeroPremiate = 0;
-         
-    for (int i = 0; i < 5; i++) {
-      if (((fotoVotiGiudici[i] % 2) == 0) && (fotoVotiGiudici[i] > 8)) {
-        System.out.println("voto foto " + i + ": " + fotoVotiGiudici[i]);
+public class ConcorsoFotografico
+{
+    public static void main(String[] args)
+    {
+        //dichiarazione e inizializzazione variabili
+        Scanner scannerNum = new Scanner(System.in);
         
-        fotoNumeroPremiate++;
-      }
+        int numFotoPremiate = 0;
+        int[] fotoVotiGiudici = new int[5];
+
+        //messaggio spiegazione input
+        System.out.println("Programma di inserimento di 5 foto da votare");
+
+        //inserimento voti delle foto
+        for (int i = 0; i < fotoVotiGiudici.length; i++)
+        {
+            //input voto
+            System.out.println("Inserire un voto da 1 a 10");
+            fotoVotiGiudici[i] = scannerNum.nextInt();
+
+            //controlla se il voto è un valore valido
+            while(fotoVotiGiudici[i] < 1 || fotoVotiGiudici[i] > 10)
+            {
+                //input voto
+                System.out.println("Errore: inserire un voto compreso tra 1 e 10");
+                fotoVotiGiudici[i] = scannerNum.nextInt();
+            }
+        }
+
+        for (int i = 0; i < fotoVotiGiudici.length; i++)
+        {
+            //controlla se ci sono foto premiate
+            if (((fotoVotiGiudici[i] % 2) == 0) && (fotoVotiGiudici[i] >= 8))
+            {
+                //stampa a schermo il voto della foto premiata
+                System.out.println("Foto n." + i + " premiata con il voto: " + fotoVotiGiudici[i]);
+
+                //conteggio delle foto premiate
+                numFotoPremiate++;
+            }
+        }
+
+        // stampare numero totale foto premiate
+
+        // stampare concorso annullato se nessuna foto riceve almeno 6 punti
+
+        //chiusura dell'utilizzo dello scanner
+        scannerNum.close();
     }
-      
-
-    // stampare numero totale foto premiate
-
-
-
-
-
-    // stampare concorso annullato se nessuna foto riceve almeno 6 punti
-
-
-
-    scannerNum.close();
-  }
 }
