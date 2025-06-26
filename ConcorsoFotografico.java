2aa452deaea83b68d38ec490f0d4e9da9a224482
Davide Latina <davide.latina616@gmail.com>
2025-06-26 17:07:35 +0200

    Update ConcorsoFotografico.java

--- ConcorsoFotografico.java
+++ ConcorsoFotografico.java
@@ -2,29 +2,35 @@ package ConcorsoFotograficoTeam2;
 
 import javax.sound.sampled.SourceDataLine;
 
+import java.util.Scanner;
+
 public class ConcorsoFotografico {
-  // memorizza voti da 1 a 10 assegnati a 5 foto
-  // usa un ciclo for per inserire i voti uno alla volta
-
- 
-
-
-
-  // stampare quali foto sono state premiate, se con voto >= a 8 (con operatore %)
-
+  public static void main(String[] args) {
+    // memorizza voti da 1 a 10 assegnati a 5 foto
+    // usa un ciclo for per inserire i voti uno alla volta
+    
+    Scanner scannerNum = new Scanner(System.in);
+    
+    int[][] fotoVotiGiudici = new int[5][5];
 
 
 
-
-  // stampare numero totale foto premiate
+    // stampare quali foto sono state premiate, se con voto >= a 8 (con operatore %)
 
 
 
 
 
-  // stampare concorso annullato se nessuna foto riceve almeno 6 punti
+    // stampare numero totale foto premiate
 
 
 
 
+
+    // stampare concorso annullato se nessuna foto riceve almeno 6 punti
+
+
+
+    scannerNum.close();
+  }
 }
